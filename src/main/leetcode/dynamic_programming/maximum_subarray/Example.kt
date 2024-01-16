304114fafa182351e2774abf7d4b9572f74482e2
masu <rin090506@ezweb.ne.jp>
2024-01-17 00:20:17 +0900

    53. Maximum Subarray 別解

--- src/main/leetcode/dynamic_programming/maximum_subarray/Example.kt
+++ src/main/leetcode/dynamic_programming/maximum_subarray/Example.kt
@@ -15,3 +15,15 @@ fun maxSubArray(nums: IntArray): Int {
 
     return maxOfAll
 }
+
+fun maxSubArray2(nums: IntArray): Int {
+    var currentMax = nums[0]
+    var maxOfAll = nums[0]
+
+    for( i in 1 until nums.size ) {
+        currentMax = if(nums[i] > currentMax + nums[i]) nums[i] else currentMax + nums[i]
+        maxOfAll = if(maxOfAll > currentMax) maxOfAll else currentMax
+    }
+
+    return maxOfAll
+}
