d958cb50afc2ea31599d3bc5a9bb1f2ccf1effbe
Raomatsu <rin090506@gmail.com>
2024-01-22 00:42:43 +0900

    122. Best Time to Buy and Sell Stock II

--- src/main/leetcode/dynamic_programming/house_robber/Example.kt
+++ src/main/leetcode/dynamic_programming/house_robber/Example.kt
@@ -1,2 +1,16 @@
 package leetcode.dynamic_programming.house_robber
 
+fun main() {
+
+}
+fun maxProfit(prices: IntArray): Int {
+    var profit = 0
+
+    for (i in 0..prices.size - 2) {
+        // 次要素都のが大きければ差分+1
+        val diff = prices[i + 1] - prices[i]
+        if (diff > 0) profit += diff
+    }
+
+    return profit
+}
