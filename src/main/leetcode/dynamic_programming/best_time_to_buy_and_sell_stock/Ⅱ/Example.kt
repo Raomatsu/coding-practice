3f6e4372cb8f67c81fc97da277cccc00dd7994c8
Raomatsu <rin090506@gmail.com>
2024-02-02 23:54:23 +0900

    122. Best Time to Buy and Sell Stock

---
+++ src/main/leetcode/dynamic_programming/best_time_to_buy_and_sell_stock/Ⅱ/Example.kt
@@ -0,0 +1,2 @@
+package leetcode.dynamic_programming.best_time_to_buy_and_sell_stock.`Ⅱ`
+
--- src/main/leetcode/dynamic_programming/maximum_subarray/Example.kt
+++ src/main/leetcode/dynamic_programming/maximum_subarray/Example.kt
@@ -4,38 +4,13 @@ fun main() {
 
 }
 
-fun maxSubArray(nums: IntArray): Int {
-    var currentMax = nums[0]
-    var maxOfAll = nums[0]
+fun maxProfit(prices: IntArray): Int {
+    var profit = 0
 
-    for( i in 1 until nums.size ) {
-        currentMax = Math.max(nums[i], currentMax + nums[i])
-        maxOfAll = Math.max(maxOfAll, currentMax)
+    for (i in 0..prices.size - 2) {
+        val diff = prices[i + 1] - prices[i]
+        if (diff > 0) profit += diff
     }
 
-    return maxOfAll
-}
-
-fun maxSubArray2(nums: IntArray): Int {
-    var currentMax = nums[0]
-    var maxOfAll = nums[0]
-
-    for( i in 1 until nums.size ) {
-        currentMax = if(nums[i] > currentMax + nums[i]) nums[i] else currentMax + nums[i]
-        maxOfAll = if(maxOfAll > currentMax) maxOfAll else currentMax
-    }
-
-    return maxOfAll
-}
-
-fun maxSubArray3(nums: IntArray): Int {
-    var currentMax = nums[0]
-    var maxOfAll = nums[0]
-
-    for( i in 1 until nums.size ) {
-        currentMax = if(nums[i] > currentMax + nums[i]) nums[i] else currentMax + nums[i]
-        maxOfAll = if(maxOfAll > currentMax) maxOfAll else currentMax
-    }
-
-    return maxOfAll
+    return profit
 }
